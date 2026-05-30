ce0f8903a6e47c7d07ad25d392fe244a357bab23
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-05-30 07:30:52 +0530

    Added terraform files of dev environment

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,38 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.environment}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
