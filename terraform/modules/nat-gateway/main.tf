ac7b59300786674af4ddd1d31734d0742cfff4cf
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-07-27 05:28:52 +0530

    Added NAT gateway

--- terraform/modules/nat-gateway/main.tf
+++ terraform/modules/nat-gateway/main.tf
@@ -14,3 +14,9 @@ resource "aws_nat_gateway" "this" {
     Name = "${var.environment}-nat-gateway"
   }
 }
+
+resource "aws_route" "private_app_nat_route" {
+  route_table_id         = var.private_app_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
--- terraform/modules/nat-gateway/variables.tf
+++ terraform/modules/nat-gateway/variables.tf
@@ -2,10 +2,6 @@ variable "public_subnet_id" {
   type = string
 }
 
-variable "internet_gateway_id" {
-  type = string
-}
-
 variable "private_app_route_table_id" {
   type = string
 }
