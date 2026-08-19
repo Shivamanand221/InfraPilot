5ff8309c7c7953db95351066eba96caa1653d055
Shivam Anand <108640569+Shivamanand221@users.noreply.github.com>
2026-08-19 22:05:32 +0530

    Test CI/CD deployment v2

--- Strapi/src/api/article/controllers/article.ts
+++ Strapi/src/api/article/controllers/article.ts
@@ -1,7 +1,9 @@
 /**
- *  article controller
+ * article controller
  */
 
 import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::article.article');
+
+// CI/CD deployment test - v2
